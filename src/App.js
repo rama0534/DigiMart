48605a46962a4527f65976a52a113e3f53077d4c
NayuduRama <sadamramanayudu@gmail.com>
2021-12-07 18:44:24 +1100

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import './App.css'; 
+import LandingPage from './components/LandingPage';
+import PageNotFound from './components/PageNotFound';
+import SamplePage from './components/SamplePage';
+import Webdesign from './components/Webdesign';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <>
+      <Router>
+          <Routes>
+              <Route path="/" element={<LandingPage />}>   </Route>
+              <Route path="/sample" element={<SamplePage />} /> 
+              <Route path='/webdesign' element={<Webdesign/>} />
+              <Route path="/pagenotfound" element={<PageNotFound />}/>
+              
+          </Routes>
+      </Router>
+    </> 
   );
 }
 
